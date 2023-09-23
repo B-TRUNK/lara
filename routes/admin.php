02607d1bc18d561868b77f5d4a2c5f09f2c60041
abanob.anis <abanob.anis@fateel.co>
2023-09-23 15:15:14 +0300

    working_on_routes

---
+++ routes/admin.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register admin routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "admin" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/admin', function () {
+//     return view('admin/home');
+// }) -> name('admin.home');
+
+/* Route Namespaces -> all routes below are referring to controller under a folder
+(namespace) called 'Admin' under app*/
+Route::group((['namespace' => 'Admin' ,'prefix' => 'adm' ,'middleware' => 'web'])
+            , function ()
+{
+
+    route::get( 'admin','AdminController@showAdminname');
+
+}
+);
